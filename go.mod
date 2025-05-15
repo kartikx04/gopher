2592d8be351e247075063ce0b90c41285aa89d7b
kartik Verma <kartikx04@gmail.com>
2025-05-15 15:03:07 +0530

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kartikx04/gopher
+
+go 1.23.5
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+type server struct {
+	addr string
+}
+
+func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Server says hello!"))
+}
+
+func main() {
+	s := &server{addr: ":8080"}
+	if err := http.ListenAndServe(s.addr, s); err != nil {
+		log.Fatal(err)
+	}
+}
